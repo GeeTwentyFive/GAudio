c3672faf23265fbd3a85d0cd370a5f8ca662685b
Gee <alickoamarko@gmail.com>
2026-04-27 23:58:41 +0200

    WIP

--- GAudio.cpp
+++ GAudio.cpp
@@ -5,42 +5,42 @@
 
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 
 #define ERROR(msg) throw std::runtime_error(std::string("[ERROR] ") + __FILE__ + "@" + std::to_string(__LINE__) + " (" + __func__ + "): " + (msg))
 
 
-ma_engine* engine;
+ma_engine engine;
 
 
 GAudio::GAudio() { ma_result result;
-        result = ma_engine_init(NULL, engine); if (result != MA_SUCCESS) ERROR(std::string("Failed to initialize miniaudio engine - ") + std::to_string(result));
+        result = ma_engine_init(NULL, &engine); if (result != MA_SUCCESS) ERROR(std::string("Failed to initialize miniaudio engine - ") + std::to_string(result));
 }
 
-void GAudio::PlayFile(
+std::vector<ma_sound> active_sounds;
+void GAudio::Play(
         const char* audio_file_path,
         float pos_x = 0.0, float pos_y = 0.0, float pos_z = 0.0
 ) { ma_result result;
-        ma_sound sound; result = ma_sound_init_from_file(engine, audio_file_path, MA_SOUND_FLAG_DECODE, NULL, NULL, &sound); if (result != MA_SUCCESS) ERROR(std::string("Failed to initialize miniaudio sound - ") + std::to_string(result));
+        ma_sound sound; result = ma_sound_init_from_file(&engine, audio_file_path, MA_SOUND_FLAG_DECODE, NULL, NULL, &sound); if (result != MA_SUCCESS) ERROR(std::string("Failed to initialize miniaudio sound - ") + std::to_string(result));
         ma_sound_set_position(&sound, pos_x, pos_y, pos_z);
         result = ma_sound_start(&sound); if (result != MA_SUCCESS) ERROR(std::string("Failed to play miniaudio sound - ") + std::to_string(result));
+        active_sounds.push_back(sound);
+
+        // Clean up ended sounds
+        for (auto sound_it = active_sounds.begin(); sound_it != active_sounds.end(); ) {
+                if (!ma_sound_at_end(&(*sound_it))) { sound_it++; continue; }
+                ma_sound_uninit(&(*sound_it));
+                delete &(*sound_it);
+                sound_it = active_sounds.erase(sound_it);
+        }
 }
 
-void GAudio::SetListenerPosition(float x, float y, float z) { ma_engine_listener_set_position(engine, 0, x, y, z); }
-void GAudio::SetListenerRotation(float yaw) { ma_engine_listener_set_direction(engine, 0, sin(yaw), 0, cos(yaw)); }
+void GAudio::SetListenerPosition(float x, float y, float z) { ma_engine_listener_set_position(&engine, 0, x, y, z); }
+void GAudio::SetListenerRotation(float yaw) { ma_engine_listener_set_direction(&engine, 0, sin(yaw), 0, cos(yaw)); }
 
-void GAudio::RecordMicrophone(MicrophoneDataCallback data_callback) {
-        // TODO
+GAudio::~GAudio() {
+        for (ma_sound& sound : active_sounds) { ma_sound_uninit(&sound); delete &sound; }
+        ma_engine_uninit(&engine);
 }
-
-void GAudio::PlayRaw(
-        void* pcm_frames, unsigned int pcm_frames_count,
-        GAudio::Format format,
-        unsigned int channels,
-        unsigned int sample_rate,
-        float pos_x = 0.0, float pos_y = 0.0, float pos_z = 0.0
-) {
-        // TODO
-}
-
-GAudio::~GAudio() { ma_engine_uninit(engine); }
--- GAudio.hpp
+++ GAudio.hpp
@@ -4,37 +4,13 @@ class GAudio {
 public:
         GAudio();
 
-        void PlayFile(
+        void Play(
                 const char* audio_file_path,
                 float pos_x = 0.0, float pos_y = 0.0, float pos_z = 0.0
         );
 
         void SetListenerPosition(float x, float y, float z);
         void SetListenerRotation(float yaw);
-
-        enum class Format {
-                U8 = 1,
-                S16 = 2,
-                S24 = 3,
-                S32 = 4,
-                F32 = 5
-        };
-
-        typedef void(*MicrophoneDataCallback)(
-                void* pcm_frames, unsigned int pcm_frames_count,
-                Format format,
-                unsigned int channels,
-                unsigned int sample_rate
-        );
-        void RecordMicrophone(MicrophoneDataCallback data_callback);
-
-        void PlayRaw(
-                void* pcm_frames, unsigned int pcm_frames_count,
-                Format format,
-                unsigned int channels,
-                unsigned int sample_rate,
-                float pos_x = 0.0, float pos_y = 0.0, float pos_z = 0.0
-        );
 
         ~GAudio();
 };
