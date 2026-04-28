95db75d1490bf1d3f9588dab54be369f6d3df8d7
Gee <alickoamarko@gmail.com>
2026-04-28 21:25:19 +0200

    WIP: Interface

--- GAudio.hpp
+++ GAudio.hpp
@@ -1,16 +1,53 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 class GAudio {
 public:
         GAudio();
 
-        void Play(
-                const char* audio_file_path,
-                float pos_x = 0.0, float pos_y = 0.0, float pos_z = 0.0
-        );
+        enum class Format : uint8_t { U8, S16, S24, S32, F32 };
 
-        void SetListenerPosition(float x, float y, float z);
-        void SetListenerRotation(float yaw);
+        class SoundStream {
+        public:
+                SoundStream(
+                        Format format,
+                        uint32_t channels,
+                        uint32_t sample_rate
+                );
+                void SetVolume(float volume);
+                void SetLooping(bool on);
+                void SetSpatialization(bool on);
+                void SetPosition3D(float x, float y, float z);
+                void SetVelocity3D(float x, float y, float z);
+                void SubmitPCM(const void* pcm_frames, uint32_t pcm_frames_count);
+                ~SoundStream();
+        };
+
+        class Sound : public SoundStream {
+        public:
+                Sound(
+                        void* pcm_frames, uint32_t pcm_frames_count,
+                        Format format,
+                        uint32_t channels,
+                        uint32_t sample_rate
+                );
+                void Play();
+                ~Sound();
+        };
+
+        void LoadSoundFileOrFiles(std::string sound_file_or_dir_path);
+        Sound CreateSoundFromLoadedFile(std::string loaded_sound_file_path);
+
+        void Set3DAttenuation(float min_distance, float max_distance);
+
+        // f32, 48khz, 1 channel    (converted if different)
+        std::vector<float> PopMicrophoneData();
+
+        void SetListenerPosition3D(float x, float y, float z);
+        void SetListenerRotation3D(float yaw);
+        void SetListenerVelocity3D(float x, float y, float z);
 
         ~GAudio();
 };
