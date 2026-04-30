f2ed886822bf2e51dbc403ff59b906dc9313a5e0
Gee <alickoamarko@gmail.com>
2026-04-30 18:07:12 +0200

    WIP

--- GAudio.cpp
+++ GAudio.cpp
@@ -12,18 +12,50 @@
 
 
 #define DEVICE_IO_SAMPLE_RATE 48000
+#define MIC_BUFFER_SIZE 256
 
 
 #define ERROR(msg) throw std::runtime_error(std::string("[ERROR] ") + __FILE__ + "@" + std::to_string(__LINE__) + " (" + __func__ + "): " + (msg))
 
+#define _MA_SOUND_FLAGS (MA_SOUND_FLAG_DECODE | MA_SOUND_FLAG_NO_PITCH | MA_SOUND_FLAG_NO_SPATIALIZATION)
+
 
 ma_engine engine;
+ma_device mic;
+bool capturing_mic = false;
 
 
 GAudio::GAudio() { ma_result result; ma_engine_config engine_config = ma_engine_config_init();
         engine_config.sampleRate = DEVICE_IO_SAMPLE_RATE;
         result = ma_engine_init(&engine_config, &engine); if (result != MA_SUCCESS) ERROR(std::string("Failed to initialize miniaudio engine - ") + std::to_string(result));
+
+        ma_device_config mic_config = ma_device_config_init(ma_device_type_capture);
+        mic_config.capture.format = ma_format_f32;
+        mic_config.capture.channels = 1;
+        mic_config.sampleRate = DEVICE_IO_SAMPLE_RATE;
+        mic_config.dataCallback = [](ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
+                // TODO: Write to ring buffer
+        };
+        if (ma_device_init(NULL, &mic_config, &mic) == MA_SUCCESS) {
+                if (ma_device_start(&mic) != MA_SUCCESS) ma_device_uninit(&mic);
+                else capturing_mic = true;
+        }
 }
+
+void GAudio::_Sound::SetVolume(float volume) { ma_sound_set_volume(this->sound, volume); }
+void GAudio::_Sound::SetPosition3D(float x, float y, float z) { ma_sound_set_spatialization_enabled(sound, true); ma_sound_set_position(this->sound, x, y, z); }
+GAudio::_Sound::~_Sound() { ma_sound_uninit(sound); }
+
+std::unordered_map<std::string, ma_sound> loaded_sound_files_data;
+void LoadSoundFileData(std::string sound_file_or_dir_path) {
+        // TODO: Recurse if dir -> for each file: ma_sound_init_from_file() + _MA_SOUND_FLAGS
+}
+GAudio::SoundFile::SoundFile(std::string loaded_sound_file_path) { ma_result result;
+        if (loaded_sound_files_data.find(loaded_sound_file_path) == loaded_sound_files_data.end()) ERROR(std::string("Tried to create instance of unloaded sound file ") + loaded_sound_file_path);
+        result = ma_sound_init_copy(&engine, &loaded_sound_files_data[loaded_sound_file_path], _MA_SOUND_FLAGS, NULL, sound); if (result != MA_SUCCESS) ERROR(std::string("Failed to create instance of loaded sound file ") + loaded_sound_file_path);
+}
+void GAudio::SoundFile::Play(bool loop) { ma_sound_seek_to_pcm_frame(sound, 0); ma_sound_set_looping(sound, loop); ma_sound_start(sound); }
+void GAudio::SoundFile::Stop() { ma_sound_stop(sound); }
 
 typedef struct {
         ma_data_source_base base;
@@ -32,16 +64,16 @@ typedef struct {
         ma_uint32 sample_rate;
         void* pcm_frames;
         uint32_t pcm_frames_count;
-} StreamSource;
+} StreamDataSource;
 // TODO: ^ vtable
-inline StreamSource StreamSource_init() { ma_result result;
-        StreamSource out;
+inline StreamDataSource StreamDataSource_init() { ma_result result;
+        StreamDataSource out;
         ma_data_source_config base_config = ma_data_source_config_init();
         // TODO: base_config.vtable =
         result = ma_data_source_init(&base_config, &out.base); if (result != MA_SUCCESS) ERROR(std::string("Failed to initialize miniaudio custom data source - ") + std::to_string(result));
         return out;
 }
-inline void SteamSource_uninit(StreamSource* stream_source) { ma_data_source_uninit(&stream_source->base); }
+inline void StreamDataSource_uninit(StreamDataSource* stream_source) { ma_data_source_uninit(&stream_source->base); }
 GAudio::SoundStream::SoundStream(
         GAudio::Format format = GAudio::Format::F32,
         uint32_t channels = 1,
@@ -49,22 +81,12 @@ GAudio::SoundStream::SoundStream(
 ) {
         // TODO: data source -> ma_sound
 }
-void GAudio::SoundStream::SetVolume(float volume) {} // TODO
-void GAudio::SoundStream::SetPosition3D(float x, float y, float z) {} // TODO
 void GAudio::SoundStream::SubmitPCM(const void* pcm_frames, uint32_t pcm_frames_count) {} // TODO
 GAudio::SoundStream::~SoundStream() {} // TODO
 
-std::unordered_map<std::string, ma_sound> loaded_sound_files_data;
-void LoadSoundFileData(std::string sound_file_or_dir_path) {
-        // TODO: ma_sound_init_from_file() + (MA_SOUND_FLAG_DECODE | MA_SOUND_FLAG_NO_PITCH)
-}
-GAudio::SoundFile::SoundFile(std::string loaded_sound_file_path) {
-        // TODO
-}
-void GAudio::SoundFile::Play(bool loop) {} // TODO: ma_sound_seek_to_pcm_frame(&sound, 0) + ma_sound_set_looping()
-GAudio::SoundFile::~SoundFile() {} // TODO
-
 std::vector<float> GAudio::PopMicrophoneData() {
+        if (!capturing_mic) return std::vector<float>(0);
+        std::vector<float> mic_data; mic_data.reserve(MIC_BUFFER_SIZE);
         // TODO: Pop from mic ring buffer
 }
 
--- GAudio.hpp
+++ GAudio.hpp
@@ -1,23 +1,26 @@
 #pragma once
 
+struct ma_sound; typedef struct ma_sound ma_sound;
+
 #include <string>
 #include <vector>
 
 class GAudio { public: GAudio(); ~GAudio();
-        class ISound { public: ISound(){} virtual ~ISound(){}
-                virtual void SetVolume(float volume) = 0;
-                virtual void SetPosition3D(float x, float y, float z) = 0; // Spatialization is disabled by default unless this is called
-        }; // TODO: Refactor into not an interface
+        class _Sound { protected: ma_sound* sound = 0; _Sound(){}; public: ~_Sound();
+                void SetVolume(float volume);
+                void SetPosition3D(float x, float y, float z); // Spatialization is disabled by default unless this is called
+        };
 
         void LoadSoundFileData(std::string sound_file_or_dir_path);
-        class SoundFile : public ISound { public: ~SoundFile(); void SetVolume(float volume); void SetPosition3D(float x, float y, float z);
+        class SoundFile : public _Sound { public: ~SoundFile(){};
                 SoundFile(std::string loaded_sound_file_path);
                 void Play(bool loop);
+                void Stop();
         };
 
         enum class Format : uint8_t { U8, S16, S24, S32, F32 };
 
-        class SoundStream : public ISound { public: ~SoundStream(); void SetVolume(float volume); void SetPosition3D(float x, float y, float z);
+        class SoundStream : public _Sound { public: ~SoundStream();
                 SoundStream(
                         Format format = GAudio::Format::F32,
                         uint32_t channels = 1,
